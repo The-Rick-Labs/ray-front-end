22ddeb25cb7ed20fb817873d9be098084a6e28e8
Bilal Khan <bilal.khan1@uwaterloo.ca>
2020-10-13 18:02:01 -0500

    add boilerplate code

--- README.md
+++ README.md
@@ -2,7 +2,14 @@
 
 ## installation
 
-(assuming windows)
-
 - install node (https://nodejs.org/en/download/)
-- clone the repository `git clone https://github.com/bkkaggle/ray-front-end.git`
+- clone the repository `https://github.com/The-Rick-Labs/ray-front-end.git`
+- install the dependencies `npm install`
+- run with `npm start`
+
+## docs
+
+- react https://reactjs.org/
+- firebase https://firebase.google.com/docs/database/web/start?authuser=0
+- firebase db docs https://firebase.google.com/docs/database/web/start?authuser=0
+- firebase console https://console.firebase.google.com/
--- src/index.js
+++ src/index.js
@@ -1,13 +1,79 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => (
-	<h1>
-		Hello,{' '}
-		<span role='img' aria-label='wave'>
-			👋
-		</span>
-	</h1>
-)
+import * as firebase from 'firebase/app'
+import 'firebase/database'
+
+class App extends React.Component {
+	state = {
+		stress: 0,
+	}
+
+	constructor(props) {
+		super(props)
+
+		this.handleAddStress = this.handleAddStress.bind(this)
+		this.handleDecreaseStress = this.handleDecreaseStress.bind(this)
+	}
+
+	componentDidMount() {
+		var firebaseConfig = {
+			apiKey: 'AIzaSyAImG5Vk9cS8Yi_UUNX9gwO-4_b1z2KAR0',
+			authDomain: 'rayside-94e8d.firebaseapp.com',
+			databaseURL: 'https://rayside-94e8d.firebaseio.com',
+			projectId: 'rayside-94e8d',
+			storageBucket: 'rayside-94e8d.appspot.com',
+			messagingSenderId: '819405678971',
+			appId: '1:819405678971:web:74554bcb338cafdb07b5de',
+			measurementId: 'G-4JECV8ZB79',
+		}
+
+		// Initialize Firebase
+		firebase.initializeApp(firebaseConfig)
+
+		var database = firebase.database()
+		database.ref('state/stress').on('value', (snapshot) => {
+			this.setState({ stress: snapshot.val() })
+		})
+	}
+
+	handleReset() {
+		var database = firebase.database()
+		database.ref('state').set({
+			stress: 50,
+		})
+	}
+
+	handleAddStress() {
+		var database = firebase.database()
+		database.ref('state').set({
+			stress: this.state.stress + 10,
+		})
+	}
+
+	handleDecreaseStress() {
+		var database = firebase.database()
+		database.ref('state').set({
+			stress: this.state.stress - 10,
+		})
+	}
+
+	render() {
+		return (
+			<>
+				<h1>
+					Hello,{' '}
+					<span role='img' aria-label='wave'>
+						👋
+					</span>
+				</h1>
+				<button onClick={this.handleReset}>reset db</button>
+				<button onClick={this.handleAddStress}>add stress</button>
+				<button onClick={this.handleDecreaseStress}>decrease stress</button>
+				<h3>{this.state.stress}</h3>
+			</>
+		)
+	}
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
